702f51f29aa93b278cbb713f8a9b7ec6055e682f
Alex Lum <alex.lum@vtac.edu.au>
2019-09-09 09:40:53 +1000

    Switch polls to 2019-22

--- polling-graph.r
+++ polling-graph.r
@@ -1,18 +1,18 @@
 library(ggplot2)
 setwd("~/Documents/GitHub/wikipedia-aus-poll-charts")
-polling1619 <- read.csv("polling1619.csv")
-primary_votes <- ggplot(polling1619, aes(x=as.Date(last_date, '%d %b %Y'))) +
+polling1922 <- read.csv("polling1922.csv")
+primary_votes <- ggplot(polling1922, aes(x=as.Date(last_date, '%d %b %Y'))) +
   theme_bw() +
   geom_point(aes(y=(pv_lnp)), colour="blue4", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(pv_lnp), colour="LNP"), span = 0.2, se = FALSE) +
+  geom_smooth(aes(y=(pv_lnp), colour="LNP"), span = 0.2, se = TRUE) +
   geom_point(aes(y=(pv_alp)), colour="red3", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(pv_alp), colour="ALP"), span = 0.2, se = FALSE) +
+  geom_smooth(aes(y=(pv_alp), colour="ALP"), span = 0.2, se = TRUE) +
   geom_point(aes(y=(pv_grn)), colour="green4", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(pv_grn), colour="GRN"), span = 0.2, se = FALSE) +
+  geom_smooth(aes(y=(pv_grn), colour="GRN"), span = 0.2, se = TRUE) +
   geom_point(aes(y=(pv_onp)), colour="yellow3", size=2, alpha = 5/10) +
-  geom_smooth(aes(y=(pv_onp), colour="ONP"), span = 0.2, se = FALSE) +
+  geom_smooth(aes(y=(pv_onp), colour="ONP"), span = 0.2, se = TRUE) +
   geom_point(aes(y=(pv_oth)), colour="gray60", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(pv_oth), colour="OTH"), span = 0.2, se = FALSE) +
+  geom_smooth(aes(y=(pv_oth), colour="OTH"), span = 0.2, se = TRUE) +
   scale_y_continuous(limits=c(0, 50), breaks=c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50), minor_breaks = NULL) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL) +
   theme(axis.text.x = element_text(angle=45, vjust=0.5, size=12), axis.text.y = element_text(size=12), axis.title.y = element_text(size=14)) +
@@ -22,12 +22,12 @@ primary_votes <- ggplot(polling1619, aes(x=as.Date(last_date, '%d %b %Y'))) +
                      values = c("LNP"="blue4", "ALP"="red3", "GRN"="green4", "ONP"="yellow3", "OTH"="gray60"))
 primary_votes + theme(legend.position="bottom", legend.box = "horizontal", legend.text = element_text(size=12))
 
-tpp <- ggplot(polling1619, aes(x=as.Date(last_date, '%d %b %Y'))) +
+tpp <- ggplot(polling1922, aes(x=as.Date(last_date, '%d %b %Y'))) +
   theme_bw() +
   geom_point(aes(y=(tpp_lnp)), colour="blue4", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(tpp_lnp), colour="LNP"), span = 0.2, se = FALSE) +
+  geom_smooth(aes(y=(tpp_lnp), colour="LNP"), span = 0.2, se = TRUE) +
   geom_point(aes(y=(tpp_alp)), colour="red3", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(tpp_alp), colour="ALP"), span = 0.2, se = FALSE) +
+  geom_smooth(aes(y=(tpp_alp), colour="ALP"), span = 0.2, se = TRUE) +
   scale_y_continuous(limits=c(40, 60), breaks=c(42, 44, 46, 48, 50, 52, 54, 56, 58, 60), minor_breaks = NULL) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL) +
   theme(axis.text.x = element_text(angle=45, vjust=0.5, size=12), axis.text.y = element_text(size=12), axis.title.y = element_text(size=14)) +

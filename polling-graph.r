33ff113cd7d1e12d39ac214b725e810b4474cd92
metacoretechs <ajlum@iinet.net.au>
2019-11-11 07:51:35 +1100

    10 Nov Newspoll

--- polling-graph.r
+++ polling-graph.r
@@ -1,18 +1,19 @@
 library(ggplot2)
 setwd("~/Documents/GitHub/wikipedia-aus-poll-charts")
 polling1922 <- read.csv("polling1922.csv")
+spansize <- 0.5
 primary_votes <- ggplot(polling1922, aes(x=as.Date(last_date, '%d %b %Y'))) +
   theme_bw() +
   geom_point(aes(y=(pv_lnp)), colour="blue4", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(pv_lnp), colour="LNP"), span = 0.2, se = TRUE) +
+  geom_smooth(aes(y=(pv_lnp), colour="LNP"), span = spansize, se = FALSE) +
   geom_point(aes(y=(pv_alp)), colour="red3", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(pv_alp), colour="ALP"), span = 0.2, se = TRUE) +
+  geom_smooth(aes(y=(pv_alp), colour="ALP"), span = spansize, se = FALSE) +
   geom_point(aes(y=(pv_grn)), colour="green4", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(pv_grn), colour="GRN"), span = 0.2, se = TRUE) +
+  geom_smooth(aes(y=(pv_grn), colour="GRN"), span = spansize, se = FALSE) +
   geom_point(aes(y=(pv_onp)), colour="yellow3", size=2, alpha = 5/10) +
-  geom_smooth(aes(y=(pv_onp), colour="ONP"), span = 0.2, se = TRUE) +
+  geom_smooth(aes(y=(pv_onp), colour="ONP"), span = spansize, se = FALSE) +
   geom_point(aes(y=(pv_oth)), colour="gray60", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(pv_oth), colour="OTH"), span = 0.2, se = TRUE) +
+  geom_smooth(aes(y=(pv_oth), colour="OTH"), span = spansize, se = FALSE) +
   scale_y_continuous(limits=c(0, 50), breaks=c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50), minor_breaks = NULL) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL) +
   theme(axis.text.x = element_text(angle=45, vjust=0.5, size=12), axis.text.y = element_text(size=12), axis.title.y = element_text(size=14)) +
@@ -25,9 +26,9 @@ primary_votes + theme(legend.position="bottom", legend.box = "horizontal", legen
 tpp <- ggplot(polling1922, aes(x=as.Date(last_date, '%d %b %Y'))) +
   theme_bw() +
   geom_point(aes(y=(tpp_lnp)), colour="blue4", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(tpp_lnp), colour="LNP"), span = 0.2, se = TRUE) +
+  geom_smooth(aes(y=(tpp_lnp), colour="LNP"), span = spansize, se = FALSE) +
   geom_point(aes(y=(tpp_alp)), colour="red3", size=2, alpha = 3/10) +
-  geom_smooth(aes(y=(tpp_alp), colour="ALP"), span = 0.2, se = TRUE) +
+  geom_smooth(aes(y=(tpp_alp), colour="ALP"), span = spansize, se = FALSE) +
   scale_y_continuous(limits=c(40, 60), breaks=c(42, 44, 46, 48, 50, 52, 54, 56, 58, 60), minor_breaks = NULL) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL) +
   theme(axis.text.x = element_text(angle=45, vjust=0.5, size=12), axis.text.y = element_text(size=12), axis.title.y = element_text(size=14)) +
@@ -36,3 +37,4 @@ tpp <- ggplot(polling1922, aes(x=as.Date(last_date, '%d %b %Y'))) +
                      labels = c("Australian Labor Party", "Liberal-National Coalition"), 
                      values = c("LNP"="blue4", "ALP"="red3"))
 tpp + theme(legend.position="bottom", legend.box = "horizontal", legend.text = element_text(size=12))
+

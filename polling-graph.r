6318403769f8822a4e896e0e37e8e4fdbc7377b8
metacoretechs <ajlum@iinet.net.au>
2022-04-20 23:43:54 +1000

    20 April Essential

--- polling-graph.r
+++ polling-graph.r
@@ -28,20 +28,20 @@ polling1922 <- polling1922 %>%
 
 primary_votes <- ggplot(polling1922, aes(x=as.Date(last_date, '%d %b %Y'))) +
   theme_bw() +
-  geom_point(aes(y=pv_lnp), colour="blue4", size=2, alpha = 3/10) +
+  geom_point(aes(y=pv_lnp), colour="blue4", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=pv_lnp, colour="LNP"), span = spansize, se = FALSE) +
-  geom_point(aes(y=pv_alp), colour="red3", size=2, alpha = 3/10) +
+  geom_point(aes(y=pv_alp), colour="red3", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=pv_alp, colour="ALP"), span = spansize, se = FALSE) +
-  geom_point(aes(y=pv_grn), colour="green4", size=2, alpha = 3/10) +
+  geom_point(aes(y=pv_grn), colour="green4", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=pv_grn, colour="GRN"), span = spansize, se = FALSE) +
-  geom_point(aes(y=pv_onp), colour="orange3", size=2, alpha = 5/10) +
+  geom_point(aes(y=pv_onp), colour="orange3", size=2.5, alpha = 5/10) +
   geom_smooth(aes(y=pv_onp, colour="ONP"), span = spansize, se = FALSE) +
-  geom_point(aes(y=pv_uap), colour="yellow3", size=2, alpha = 5/10) +
+  geom_point(aes(y=pv_uap), colour="yellow3", size=2.5, alpha = 5/10) +
   geom_smooth(aes(y=pv_uap, colour="UAP"), span = spansize, se = FALSE) +
-  geom_point(aes(y=pv_oth), colour="gray60", size=2, alpha = 3/10) +
+  geom_point(aes(y=pv_oth), colour="gray60", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=pv_oth, colour="OTH"), span = spansize, se = FALSE) +
   scale_y_continuous(limits=c(0, 50), breaks=c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50), minor_breaks = NULL) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL, expand = c(0, 0)) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL) +
   theme(axis.text.x = element_text(angle=45, vjust=0.5, size=12), axis.text.y = element_text(size=12), axis.title.y = element_text(size=14)) +
   labs(y="Voters (%)", x= NULL) +
   scale_colour_manual(name="", 
@@ -51,9 +51,9 @@ primary_votes + theme(legend.position="bottom", legend.box = "horizontal", legen
 
 tpp <- ggplot(polling1922, aes(x=as.Date(last_date, '%d %b %Y'))) +
   theme_bw() +
-  geom_point(aes(y=tpp_lnp), colour="blue4", size=2, alpha = 3/10) +
+  geom_point(aes(y=tpp_lnp), colour="blue4", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=tpp_lnp, colour="LNP"), span = spansize, se = FALSE) +
-  geom_point(aes(y=tpp_alp), colour="red3", size=2, alpha = 3/10) +
+  geom_point(aes(y=tpp_alp), colour="red3", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=tpp_alp, colour="ALP"), span = spansize, se = FALSE) +
   scale_y_continuous(limits=c(40, 60), breaks=c(42, 44, 46, 48, 50, 52, 54, 56, 58, 60), minor_breaks = NULL, expand = c(0,0)) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL) +
@@ -66,11 +66,11 @@ tpp + theme(legend.position="bottom", legend.box = "horizontal", legend.text = e
 
 ppm <- ggplot(ppm1922, aes(x=as.Date(date, '%d-%b-%y'))) +
   theme_bw() +
-  geom_point(aes(y=morrison), colour="blue4", size=2, alpha = 3/10) +
+  geom_point(aes(y=morrison), colour="blue4", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=morrison, colour="Morrison"), span = spansize, se = FALSE) +
-  geom_point(aes(y=albanese), colour="red3", size=2, alpha = 3/10) +
+  geom_point(aes(y=albanese), colour="red3", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=albanese, colour="Albanese"), span = spansize, se = FALSE) +
-  geom_point(aes(y=unknown), colour="grey20", size=2, alpha = 3/10) +
+  geom_point(aes(y=unknown), colour="grey20", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=unknown, colour="Don't Know"), span = spansize, se = FALSE) +
   scale_y_continuous(limits=c(0, 70), breaks = c(10, 20, 30, 40, 50, 60), minor_breaks = NULL, expand = c(0,0)) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL) +
@@ -83,11 +83,11 @@ ppm + theme(legend.position="bottom", legend.box = "horizontal", legend.text = e
 
 lnp_sat <- ggplot(scomosat, aes(x=as.Date(date, '%d-%b-%y'))) +
   theme_bw() +
-  geom_point(aes(y=satisfied), colour="#02e03d", size=2, alpha = 3/10) +
+  geom_point(aes(y=satisfied), colour="#02e03d", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=satisfied, colour="Satisfied"), span = spansize, se = FALSE) +
-  geom_point(aes(y=dissatisfied), colour="#f74888", size=2, alpha = 3/10) +
+  geom_point(aes(y=dissatisfied), colour="#f74888", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=dissatisfied, colour="Dissatisfied"), span = spansize, se = FALSE) +
-  geom_point(aes(y=unknown), colour="#b3b3b3", size=2, alpha = 3/10) +
+  geom_point(aes(y=unknown), colour="#b3b3b3", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=unknown, colour="Don't Know"), span = spansize, se = FALSE) +
   scale_y_continuous(limits=c(0, 70), breaks = c(0, 10, 20, 30, 40, 50, 60, 70), minor_breaks = NULL, expand = c(0,0)) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL) +
@@ -100,11 +100,11 @@ lnp_sat + theme(legend.position="bottom", legend.box = "horizontal", legend.text
 
 alp_sat <- ggplot(albosat, aes(x=as.Date(date, '%d-%b-%y'))) +
   theme_bw() +
-  geom_point(aes(y=satisfied), colour="#02e03d", size=2, alpha = 3/10) +
+  geom_point(aes(y=satisfied), colour="#02e03d", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=satisfied, colour="Satisfied"), span = spansize, se = FALSE) +
-  geom_point(aes(y=dissatisfied), colour="#f74888", size=2, alpha = 3/10) +
+  geom_point(aes(y=dissatisfied), colour="#f74888", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=dissatisfied, colour="Dissatisfied"), span = spansize, se = FALSE) +
-  geom_point(aes(y=unknown), colour="#b3b3b3", size=2, alpha = 3/10) +
+  geom_point(aes(y=unknown), colour="#b3b3b3", size=2.5, alpha = 3/10) +
   geom_smooth(aes(y=unknown, colour="Don't Know"), span = spansize, se = FALSE) +
   scale_y_continuous(limits=c(0, 70), breaks = c(0, 10, 20, 30, 40, 50, 60, 70), minor_breaks = NULL, expand = c(0,0)) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = NULL) +
